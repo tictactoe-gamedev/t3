8611cce71ecdd018e7bcce9dea475ac4382ac912
kemal <kemal@simulakra.de>
2024-01-09 17:30:58 +0300

    list resize

--- framework/library/data_structures/list.cpp
+++ framework/library/data_structures/list.cpp
@@ -1,6 +1,8 @@
 #include "list.h"
 #include "helper/memory.h"
 
+inline void T3_List_ResizeControl(T3_List *list);
+
 T3_List *T3_List_Init(uint initialCapacity) {
     T3_MallocSafe(T3_List, list)
     list->Capacity = initialCapacity;
@@ -9,4 +11,16 @@ T3_List *T3_List_Init(uint initialCapacity) {
     list->_removedIndices = (uint *) malloc(sizeof(uint) * initialCapacity / 2);
     list->_removedIndicesSize = 0;
     return list;
+}
+
+void T3_List_Add(T3_List *list, void *element) {
+    T3_List_ResizeControl(list);
+}
+
+
+void T3_List_ResizeControl(T3_List *list) {
+    if (list->Size * 2 > list->Capacity) {
+        list->Capacity *= 2;
+        list->Array = (void **) realloc(list->Array, sizeof(void *) * list->Capacity);
+    }
 }
