90493b916e80df58e77a85e484141838023f6c52
kemal <kemal@simulakra.de>
2024-01-10 20:37:12 +0300

    Issue #15: - minor bug fix

--- framework/components/position.cpp
+++ framework/components/position.cpp
@@ -10,3 +10,18 @@ T3_Comp_Register_Begin(T3_Component_Position)
     
 T3_Comp_Register_End
 
+T3_Comp_Enter_Begin(T3_Component_Position)
+
+T3_Comp_Enter_End
+
+T3_Comp_Loop_Begin(T3_Component_Position)
+
+T3_Comp_Loop_End
+
+T3_Comp_Exit_Begin(T3_Component_Position)
+
+T3_Comp_Exit_End
+
+T3_Comp_Destroy_Begin(T3_Component_Position)
+
+T3_Comp_Destroy_End
