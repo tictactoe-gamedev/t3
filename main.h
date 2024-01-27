3e71e0b5b05e84a386b4d00e5981c3a5b0572b26
kemal <kemal@simulakra.de>
2024-01-28 02:54:51 +0300

    minor cleanup

--- main.h
+++ main.h
@@ -3,17 +3,11 @@
 
 #include "SDL.h"
 #include "SDL_video.h"
-//#include "library/ecs/entity.h"
 
-/** 
- * Anything that should be globally reachable from the game loop should be here
- */
-
-//void T3_Entity_SetMain(T3_Entity* entity);
 SDL_Window* MainWindow = NULL;
 SDL_Renderer* MainRenderer = NULL;
-
 float DeltaTimeInSeconds = 0;
+
 
 
 #endif //T3_MAIN_H
